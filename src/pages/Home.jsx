8a4cb56e4a70727a95296ffeec4abf9d8f73e7af
homirzayi <homirzayi@hotmail.com>
2023-11-14 18:32:59 +0330

    change icon and browser icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ function Home() {
     <>
       <button
         style={{ border: "2px solid #02bfbb" }}
-        className="p-2 flex justify-center items-center mx-auto mt-28"
+        className="px-8 py-4 flex justify-center items-center mx-auto mt-28 hover:bg-green-700 rounded-md text-xl"
       >
-        <a href="https://zeeg.me/pstaebi">رزرو نوبت</a>
+        <a href="https://zeeg.me/pstaebi" target="_blank" rel="noreferrer">
+          رزرو نوبت
+        </a>
       </button>
     </>
   );

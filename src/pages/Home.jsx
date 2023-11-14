22dcde5c0b14cfb4046842a7475b33a57889b5bf
homirzayi <homirzayi@hotmail.com>
2023-11-14 12:43:40 +0330

    add reserve button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <button
+        style={{ border: "2px solid #02bfbb" }}
+        className="p-2 flex justify-center items-center mx-auto mt-28"
+      >
+        <a href="https://zeeg.me/pstaebi">رزرو نوبت</a>
+      </button>
+    </>
+  );
 }
 
 export default Home;
